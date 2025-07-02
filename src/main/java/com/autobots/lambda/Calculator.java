32d553e941c87867285336b219b69274d00e1127
Altynai <madakeevaaltyna@gmail.com>
2025-07-01 23:02:31 -0700

    BankAppPro classes

---
+++ src/main/java/com/autobots/lambda/Calculator.java
@@ -0,0 +1,22 @@
+package com.autobots.lambda;
+
+public class Calculator {
+
+
+        int sum(int a, int b) {
+            return a + b;
+        }
+
+        int sub(int a, int b) {
+            return a - b;
+        }
+
+        int mul(int a, int b) {
+            return a * b;
+        }
+
+        int div(int a, int b) {
+            return a / b;
+        }
+    }
+
---
+++ src/main/java/com/autobots/lambda/MathOperation.java
@@ -0,0 +1,6 @@
+package com.autobots.lambda;
+@FunctionalInterface
+public interface MathOperation {
+
+    int operation(int a, int b);
+}
