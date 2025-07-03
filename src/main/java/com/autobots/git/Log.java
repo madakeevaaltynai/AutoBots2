4e902bd12e01df72551b48f4509b4f00b639ae78
Altynai <madakeevaaltyna@gmail.com>
2025-07-02 20:42:14 -0700

    Git3

--- src/main/java/com/autobots/git/Log.java
+++ src/main/java/com/autobots/git/Log.java
@@ -2,3 +2,12 @@ package com.autobots.git;
 
 public class Log {
 }
+//git init
+//git config user.name "Sultanbek"
+//git config user.name
+//git config user.email "sultanbek.asakeev@gmail.com"
+//git config user.email
+//git status
+//git add * - добавляет всё
+//git commit -m "BankAppPro classes"
+//git log --oneline
