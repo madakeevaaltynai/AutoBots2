78967c1d832a5b97442cf9f510239c4b08a3bb1a
Altynai <madakeevaaltyna@gmail.com>
2025-07-07 20:22:53 -0700

    Version 1.0.0

--- src/main/java/com/autobots/summer/Version.java
+++ src/main/java/com/autobots/summer/Version.java
@@ -3,5 +3,8 @@ package com.autobots.summer;
 public class Version {
     public static void main(String[] args) {
         System.out.println("version 4.0.0");
+        System.out.println("Version.1.0.1");
+        System.out.println("Version.1.0.2");
+        System.out.println("Version.1.0.3------> Bug");
     }
 }
